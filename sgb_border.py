ff6e4ba12cddf14ae4a04112b2fb5caad7397327
Martin 'A-Bone' Ahrnbom <m.ahrnbom@outlook.com>
2019-05-28 22:30:36 +0200

    more work on sgb border python script

--- sgb_border.py
+++ sgb_border.py
@@ -17,16 +17,46 @@ def max_insert(l, e, m):
         return False
 
 colors = [] # all the colors in the image. Can be at most 16
-tile_data1 = [] # max 128 elements
-tile_data2 = [] # max 128 elements 
-
+tile_data = [] # max 256 elements   
 tile_map = [] # max 1024 elements 
+
+colors.append( (0,0,0) ) # This color should be first. It's used for transparency.
+tile_data.append( [0]*64 ) # This tile should be first. It's used for transparency.
 
 # Check dimensions of image 
 assert(im.size == (256,256))
 
-# Loop through image to collect blocks 
+# Loop through image to collect blocks
 for iy in range(0, 256, 8):
     for ix in range(0, 256, 8):
-        pass 
         
+        # Loop through 8x8 block 
+        tile = []
+        
+        for block_y in range(iy, iy+8):
+            for block_x in range(ix, ix+8):
+                col = im.getpixel((block_x, block_y))
+                
+                if not col in colors:
+                    if max_insert(colors, col, 17):
+                        pass 
+                    else:
+                        raise ValueError('Too many unique colors in the image')
+                
+                col_index = colors.index(col)
+                tile.append(col_index)
+                
+        if not tile in tile_data:
+            if max_insert(tile_data, tile, 256):
+                pass
+            else:
+                raise ValueError('Too many unique tiles in the image')
+        
+        tile_index = tile_data.index(tile)
+        tile_map.append(tile_index)
+
+print("Unique colors used: {}".format(len(colors)))
+print("Unique tiles used: {}".format(len(tile_data)))
+
+assert(len(tile_map) == 32*32) # Otherwise the image is of incorrect size or something went wrong 
+
